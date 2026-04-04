dc86f2c59a5c9121960abf98497fb9387e03c7e1
unknown <dwijeshu003@gmail.com>
2026-04-04 16:21:08 +1100

    Change default port from 3001 to 10000 for Render

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const nodemailer = require('nodemailer');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
